c671c9329cfd6075c7a4fa37c28c85f2b1e724c9
Gabriel Florit <gabrielflorit@gmail.com>
2016-06-23 21:12:29 -0400

    Extract blocks

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # pif-parser
 
 Parse FCC Public Inspection Files PDFs, e.g. https://stations.fcc.gov/station-profile/wmur-tv/political-files/browse-%3E2016-%3Efederal-%3Eus_senate-%3Ekelly_ayotte
+
+## setup
+
+- `npm install -g pif-parser`
+
+## usage
+
+- coming soon
--- src/wmur.js
+++ src/wmur.js
@@ -1,7 +1,41 @@
+import { EOL } from 'os'
+import _ from 'lodash'
 
-// expect that this will take in a pdf text
+const extractBlocks = (lines) => {
+
+	// first phase: get beginning lines
+	const beginningIndices = lines.map((line, index) => ({
+		line,
+		index,
+	}))
+	.filter(value => /^\s*Start\/End/.test(value.line))
+	.map(value => value.index)
+
+	// second phase: slice lines into many "outer" blocks
+	const outerBlocks = beginningIndices.map((index, i, array) => {
+		return i === array.length - 1 ?
+			lines.slice(index) :
+			lines.slice(index, array[i + 1] - 1)
+	})
+
+	// third phase: get outer block ending lines
+	const endingIndices = outerBlocks.map(block =>
+		_.findIndex(block, line => /^\s*$/.test(line)))
+
+	// fourth phase: slice outer blocks into blocks
+	const blocks = endingIndices.map((index, i, array) =>
+		outerBlocks[i].slice(0, index))
+
+	return blocks
+
+}
+
 export default (text) => {
 
-	console.log('asdf')
+	const lines = text.split(EOL)
+
+	const blocks = extractBlocks(lines)
+
+	return blocks
 
 }
